959d3f625855276717d330c3313d83ee7edab21f
poday98 <poday736.98@gmail.com>
2021-11-27 17:26:37 -0600

    Routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,9 @@
 import Layout from "../Components/Layout/Layout";
+import Error from "../Pages/error";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import Tasks from "../Pages/Tasks/Tasks";
 
 
 const routes = [
@@ -9,11 +14,26 @@ const routes = [
 		routes: [
 			{
 				path: '/',
-				component: "",
+				component: Home,
 				exact: true,
 			},
 			{
-				component: ""
+				path: '/login',
+				component: Login,
+				exact: true,
+			},
+			{
+				path: '/register',
+				component: Register,
+				exact: true,
+			},
+			{
+				path: '/tasks',
+				component: Tasks,
+				exact: true,
+			},
+			{
+				component: Error
 			}
 		]
 	}
